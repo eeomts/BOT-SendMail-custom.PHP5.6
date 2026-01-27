6bb199cd322c8d0b167677066e1fdfeb8f44c6d2
eeomts <mateolimoreira7@gmail.com>
2026-01-27 10:04:35 -0300

    exaples

---
+++ config/emailCredentials.example.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+return [
+    'host' => 'your host',
+    'SMTPauth' => true,
+    'SMTPdebug' => 0,
+    'userName' => 'username',
+    'pass' => 'yourpass',
+    'sender' => 'youremail@email.com',
+    'SMTPSecure' => 'tls',
+    'port' => /*your port*/ 1,
+    'Mailer' => 'smtp',
+    'Priority' => 1,
+    'From' => 'email',
+    'fromName' => 'your name'
+];
+?>
